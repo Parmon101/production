8bd66cfcb58947003ec2b4583578f04d3fcd7fe7
Evgeny Belyakov <bfjeka101@gmail.com>
2024-03-24 22:20:47 +0300

    Feature, N-30, Add components select and avatar, update profile, routes

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,8 +1,12 @@
-import { type RouteProps } from "react-router-dom";
-import { AboutPage } from "pages/AboutPage";
+import { RouteProps } from "react-router-dom";
 import { MainPage } from "pages/MainPage";
+import { AboutPage } from "pages/AboutPage";
 import { NotFoundPage } from "pages/NotFoundPage";
 import { ProfilePage } from "pages/ProfilePage";
+
+export type AppRoutesProps = RouteProps & {
+  authOnly?: boolean;
+};
 
 export enum AppRoutes {
   MAIN = "main",
@@ -16,11 +20,11 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
   [AppRoutes.ABOUT]: "/about",
   [AppRoutes.PROFILE]: "/profile",
-  // last
+  // последний
   [AppRoutes.NOT_FOUND]: "*",
 };
 
-export const routeConfig: Record<AppRoutes, RouteProps> = {
+export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
     element: <MainPage />,
@@ -32,7 +36,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.PROFILE]: {
     path: RoutePath.profile,
     element: <ProfilePage />,
+    authOnly: true,
   },
+  // last
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath.not_found,
     element: <NotFoundPage />,
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { classNames } from "shared/lib/classNames/classNames";
+import { classNames, Mods } from "shared/lib/classNames/classNames";
 import React, {
   InputHTMLAttributes,
   memo,
@@ -6,36 +6,44 @@ import React, {
   useRef,
   useState,
 } from "react";
-import s from "./Input.module.scss";
+import cls from "./Input.module.scss";
 
 type HTMLInputProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
-  "value" | "onChange"
+  "value" | "onChange" | "readOnly"
 >;
 
 interface InputProps extends HTMLInputProps {
   className?: string;
-  value?: string;
+  value?: string | number;
   onChange?: (value: string) => void;
-  type?: string;
-  placeholder?: string;
   autofocus?: boolean;
+  readonly?: boolean;
 }
+
 export const Input = memo((props: InputProps) => {
   const {
     className,
     value,
     onChange,
-    placeholder,
     type = "text",
+    placeholder,
     autofocus,
+    readonly,
     ...otherProps
   } = props;
-
+  const ref = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [caretPosition, setCaretPosition] = useState(0);
 
-  const ref = useRef<HTMLInputElement>(null);
+  const isCaretVisible = isFocused && !readonly;
+
+  useEffect(() => {
+    if (autofocus) {
+      setIsFocused(true);
+      ref.current?.focus();
+    }
+  }, [autofocus]);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     onChange?.(e.target.value);
@@ -54,32 +62,32 @@ export const Input = memo((props: InputProps) => {
     setCaretPosition(e?.target?.selectionStart || 0);
   };
 
-  useEffect(() => {
-    if (autofocus) {
-      setIsFocused(true);
-      ref.current?.focus();
-    }
-  }, [autofocus]);
+  const mods: Mods = {
+    [cls.readonly]: readonly,
+  };
 
   return (
-    <div className={classNames(s.InputWrapper, {}, [className])}>
-      {placeholder && <div className={s.placeholder}>{`${placeholder}>`}</div>}
-      <div className={s.caretWrapper}>
+    <div className={classNames(cls.InputWrapper, {}, [className])}>
+      {placeholder && (
+        <div className={cls.placeholder}>{`${placeholder}>`}</div>
+      )}
+      <div className={cls.caretWrapper}>
         <input
           ref={ref}
           type={type}
           value={value}
           onChange={onChangeHandler}
-          className={s.input}
+          className={cls.input}
           onFocus={onFocus}
           onBlur={onBlur}
           onSelect={onSelect}
+          readOnly={readonly}
           {...otherProps}
         />
-        {isFocused && (
+        {isCaretVisible && (
           <span
-            className={s.caret}
-            style={{ left: `${caretPosition * 8}px` }}
+            className={cls.caret}
+            style={{ left: `${caretPosition * 9}px` }}
           />
         )}
       </div>
