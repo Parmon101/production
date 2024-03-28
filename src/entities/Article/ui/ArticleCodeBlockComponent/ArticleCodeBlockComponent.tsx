dc579d0e2ce4953729533fbd446892dc526184bf
Evgeny Belyakov <bfjeka101@gmail.com>
2024-03-28 07:36:24 +0300

    Feature, N-37, Add skeletot, code, article details card

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -1,17 +1,26 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTranslation } from "react-i18next";
-import s from "./ArticleCodeBlockComponent.module.scss";
+import { memo } from "react";
+import { Code } from "shared/ui/Code/Code";
+import cls from "./ArticleCodeBlockComponent.module.scss";
+import { ArticleCodeBlock } from "../../model/types/article";
 
 interface ArticleCodeBlockComponentProps {
   className?: string;
+  block: ArticleCodeBlock;
 }
 
-export const ArticleCodeBlockComponent = ({
-  className,
-}: ArticleCodeBlockComponentProps) => {
-  const { t } = useTranslation();
+export const ArticleCodeBlockComponent = memo(
+  (props: ArticleCodeBlockComponentProps) => {
+    const { className, block } = props;
+    const { t } = useTranslation();
 
-  return (
-    <div className={classNames(s.ArticleCodeBlockComponent, {}, [className])} />
-  );
-};
+    return (
+      <div
+        className={classNames(cls.ArticleCodeBlockComponent, {}, [className])}
+      >
+        <Code text={block.code} />
+      </div>
+    );
+  },
+);
