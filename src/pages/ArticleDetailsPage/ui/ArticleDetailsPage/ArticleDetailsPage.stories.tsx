0db7de0ccecf3d69ea8d707936c76a9deb835fd6
Evgeny Belyakov <bfjeka101@gmail.com>
2024-03-30 16:03:48 +0300

    Feature, N-41, Add article list,item,skeleton

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
@@ -29,6 +29,10 @@ const article: Article = {
   views: 1022,
   createdAt: "26.02.2022",
   type: [ArticleType.IT],
+  user: {
+    id: "1",
+    username: "Evgeny Bel",
+  },
   blocks: [
     {
       id: "1",
