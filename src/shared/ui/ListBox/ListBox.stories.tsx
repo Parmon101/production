d9a5f16c56dfd55718c6bf6a434dc77248f41734
Evgeny Belyakov <bfjeka101@gmail.com>
2024-03-31 18:11:05 +0300

    Feature, N-54, Add Dropdown component

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import "app/styles/index.scss";
 import { ListBox } from "./ListBox";
 
 export default {
@@ -10,6 +9,13 @@ export default {
   argTypes: {
     backgroundColor: { control: "color" },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ padding: 100 }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => (
@@ -17,4 +23,50 @@ const Template: ComponentStory<typeof ListBox> = (args) => (
 );
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+  value: "123",
+  items: [
+    { content: "1asfasfasf23", value: "123" },
+    { content: "1asfasfasf21233", value: "1232" },
+  ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+  direction: "top left",
+  value: "123",
+  items: [
+    { content: "1asfasfasf23", value: "123" },
+    { content: "1asfasfasf21233", value: "1232" },
+  ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+  direction: "top right",
+  value: "123",
+  items: [
+    { content: "1asfasfasf23", value: "123" },
+    { content: "1asfasfasf21233", value: "1232" },
+  ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+  direction: "bottom left",
+  value: "123",
+  items: [
+    { content: "1asfasfasf23", value: "123" },
+    { content: "1asfasfasf21233", value: "1232" },
+  ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+  direction: "bottom right",
+  value: "123",
+  items: [
+    { content: "1asfasfasf23", value: "123" },
+    { content: "1asfasfasf21233", value: "1232" },
+  ],
+};
