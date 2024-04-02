889a1bc180eb8a1985da176a263b572d3f50273f
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-02 09:53:37 +0300

    Feature, N-62, Add popover and notifications components

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import "app/styles/index.scss";
+import { NotificationList } from "./NotificationList";
+
+export default {
+  title: "shared/NotificationList",
+  component: NotificationList,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof NotificationList>;
+
+const Template: ComponentStory<typeof NotificationList> = (args) => (
+  <NotificationList {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
