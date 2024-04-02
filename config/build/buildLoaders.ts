e499a750bf50989bb1523758ee8e3de328cb69ec
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-02 09:21:53 +0300

    Feature, N-61, Configure babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,30 @@
-import type webpack from 'webpack';
-import { type BuildOptions } from './types/config';
-import {buildCssLoader} from './loaders/buildCssLoader';
-import {buildSvgLoader} from './loaders/buildSvgLoader';
-import {buildBabelLoader} from './loaders/buildBabelLoader';
+import webpack from 'webpack';
+import { buildCssLoader } from './loaders/buildCssLoader';
+import { BuildOptions } from './types/config';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDev } = options;
 
-  const svgLoader = buildSvgLoader()
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
+  const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
+  const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
+
+  const cssLoader = buildCssLoader(isDev);
+
+  // Если не используем тайпскрипт - нужен babel-loader
+  // const typescriptLoader = {
+  //     test: /\.tsx?$/,
+  //     use: 'ts-loader',
+  //     exclude: /node_modules/,
+  // };
 
   const fileLoader = {
-    test: /\.(png|jpe?g|gif)$/i,
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
       {
         loader: 'file-loader',
@@ -18,21 +32,12 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  const babelLoader = buildBabelLoader(options);
-
-  const cssLoader = buildCssLoader(isDev)
-
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
-
   return [
     fileLoader,
     svgLoader,
-    babelLoader,
-    typescriptLoader,
+    codeBabelLoader,
+    tsxCodeBabelLoader,
+    // typescriptLoader,
     cssLoader,
   ];
 }
