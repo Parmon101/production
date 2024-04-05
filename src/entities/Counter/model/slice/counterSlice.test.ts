3a867643accf91315e512cccf97e41ae0595973e
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-05 18:48:02 +0300

    Improvement, N-73, Add README

--- src/entities/Counter/model/slice/counterSlice.test.ts
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -1,4 +1,4 @@
-import { CounterSchema } from "@/entities/Counter";
+import { CounterSchema } from '../..';
 import { counterReducer, counterActions } from "./counterSlice";
 
 describe("counterSlice.test", () => {
