69af0d3ed02cd5f48efa3bafdb644179311efc15
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-06 08:12:59 +0300

    Refactoring, N-76, Change entities

--- src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
+++ src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import { memo, useCallback, useMemo } from "react";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { TabItem, Tabs } from "@/shared/ui/Tabs";
-import { ArticleType } from "../../model/consts/articleConsts";
+import { ArticleType } from "@/entities/Article";
 
 interface ArticleTypeTabsProps {
   className?: string;
@@ -33,14 +33,14 @@ export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
         content: t("Наука"),
       },
     ],
-    [t],
+    [t]
   );
 
   const onTabClick = useCallback(
     (tab: TabItem) => {
       onChangeType(tab.value as ArticleType);
     },
-    [onChangeType],
+    [onChangeType]
   );
 
   return (
--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -1,14 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { memo, useCallback } from "react";
 import { useSelector } from "react-redux";
-import {
-  ArticleSortField,
-  ArticleSortSelector,
-  ArticleType,
-  ArticleTypeTabs,
-  ArticleView,
-  ArticleViewSelector,
-} from "@/entities/Article";
+import { ArticleSortField, ArticleType, ArticleView } from "@/entities/Article";
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
 import { Card } from "@/shared/ui/Card";
@@ -25,6 +18,9 @@ import {
   getArticlesPageView,
 } from "../../model/selectors/articlesPageSelectors";
 import { articlesPageActions } from "../../model/slices/articlesPageSlice";
+import { ArticleSortSelector } from "@/features/ArticleSortSelector";
+import { ArticleViewSelector } from "@/features/ArticleViewSelector";
+import { ArticleTypeTabs } from "@/features/ArticleTypeTabs";
 
 interface ArticlesPageFiltersProps {
   className?: string;
@@ -50,7 +46,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     (view: ArticleView) => {
       dispatch(articlesPageActions.setView(view));
     },
-    [dispatch],
+    [dispatch]
   );
 
   const onChangeSort = useCallback(
@@ -59,7 +55,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData],
+    [dispatch, fetchData]
   );
 
   const onChangeOrder = useCallback(
@@ -68,7 +64,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData],
+    [dispatch, fetchData]
   );
 
   const onChangeSearch = useCallback(
@@ -77,7 +73,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       debouncedFetchData();
     },
-    [dispatch, debouncedFetchData],
+    [dispatch, debouncedFetchData]
   );
 
   const onChangeType = useCallback(
@@ -86,7 +82,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData],
+    [dispatch, fetchData]
   );
 
   return (
