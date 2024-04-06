f03664d98d127a1708afbf51d61538f4fb4ff869
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-06 08:24:32 +0300

    Improvement, N-77, Improvement build

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,5 @@
-import type webpack from 'webpack';
-import { type BuildOptions } from './types/config';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
@@ -15,14 +15,14 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
       filename: '[name].[contenthash].js',
       path: paths.build,
       clean: true,
-      publicPath: '/'
+      publicPath: '/',
     },
     plugins: buildPlugins(options),
     module: {
       rules: buildLoaders(options),
     },
     resolve: buildResolvers(options),
-    devtool: isDev ? 'inline-source-map' : undefined,
+    devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
