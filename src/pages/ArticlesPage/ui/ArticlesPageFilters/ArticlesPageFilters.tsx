147dfb45b4e706fbe1a5a4f95c2a12a66c61db69
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-16 20:55:09 +0300

    Improvement, N-95, Deprecated tag for old components

--- src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters.tsx
@@ -1,26 +1,26 @@
-import { useTranslation } from "react-i18next";
-import { memo, useCallback } from "react";
-import { useSelector } from "react-redux";
-import { ArticleSortField, ArticleType, ArticleView } from "@/entities/Article";
-import { classNames } from "@/shared/lib/classNames/classNames";
-import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch/useAppDispatch";
-import { Card } from "@/shared/ui/Card";
-import { Input } from "@/shared/ui/Input";
-import { SortOrder } from "@/shared/types/sort";
-import { useDebounce } from "@/shared/lib/hooks/useDebounce/useDebounce";
-import { fetchArticlesList } from "../../model/services/fetchArticlesList/fetchArticlesList";
-import cls from "./ArticlesPageFilters.module.scss";
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { ArticleSortField, ArticleType, ArticleView } from '@/entities/Article';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Card } from '@/shared/ui/deprecated/Card';
+import { Input } from '@/shared/ui/deprecated/Input';
+import { SortOrder } from '@/shared/types/sort';
+import { useDebounce } from '@/shared/lib/hooks/useDebounce/useDebounce';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import cls from './ArticlesPageFilters.module.scss';
 import {
   getArticlesPageOrder,
   getArticlesPageSearch,
   getArticlesPageSort,
   getArticlesPageType,
   getArticlesPageView,
-} from "../../model/selectors/articlesPageSelectors";
-import { articlesPageActions } from "../../model/slices/articlesPageSlice";
-import { ArticleSortSelector } from "@/features/ArticleSortSelector";
-import { ArticleViewSelector } from "@/features/ArticleViewSelector";
-import { ArticleTypeTabs } from "@/features/ArticleTypeTabs";
+} from '../../model/selectors/articlesPageSelectors';
+import { articlesPageActions } from '../../model/slices/articlesPageSlice';
+import { ArticleSortSelector } from '@/features/ArticleSortSelector';
+import { ArticleViewSelector } from '@/features/ArticleViewSelector';
+import { ArticleTypeTabs } from '@/features/ArticleTypeTabs';
 
 interface ArticlesPageFiltersProps {
   className?: string;
@@ -46,7 +46,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
     (view: ArticleView) => {
       dispatch(articlesPageActions.setView(view));
     },
-    [dispatch]
+    [dispatch],
   );
 
   const onChangeSort = useCallback(
@@ -55,7 +55,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData]
+    [dispatch, fetchData],
   );
 
   const onChangeOrder = useCallback(
@@ -64,7 +64,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData]
+    [dispatch, fetchData],
   );
 
   const onChangeSearch = useCallback(
@@ -73,7 +73,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       debouncedFetchData();
     },
-    [dispatch, debouncedFetchData]
+    [dispatch, debouncedFetchData],
   );
 
   const onChangeType = useCallback(
@@ -82,7 +82,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
       dispatch(articlesPageActions.setPage(1));
       fetchData();
     },
-    [dispatch, fetchData]
+    [dispatch, fetchData],
   );
 
   return (
@@ -100,7 +100,7 @@ export const ArticlesPageFilters = memo((props: ArticlesPageFiltersProps) => {
         <Input
           onChange={onChangeSearch}
           value={search}
-          placeholder={t("Поиск")}
+          placeholder={t('Поиск')}
         />
       </Card>
       <ArticleTypeTabs
