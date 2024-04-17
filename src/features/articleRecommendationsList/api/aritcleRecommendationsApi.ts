719919a62834a71fb157e0eedba2e410803698d9
Evgeny Belyakov <bfjeka101@gmail.com>
2024-04-17 19:22:04 +0300

    Improvement, N-101, Redesign ArticleListItem

--- src/features/articleRecommendationsList/api/aritcleRecommendationsApi.ts
+++ src/features/articleRecommendationsList/api/aritcleRecommendationsApi.ts
@@ -1,13 +1,14 @@
-import { rtkApi } from "@/shared/api/rtkApi";
-import { Article } from "@/entities/Article";
+import { rtkApi } from '@/shared/api/rtkApi';
+import { Article } from '@/entities/Article';
 
 const recommendationsApi = rtkApi.injectEndpoints({
   endpoints: (build) => ({
     getArticleRecommendationsList: build.query<Article[], number>({
       query: (limit) => ({
-        url: "/articles",
+        url: '/articles',
         params: {
           _limit: limit,
+          _expand: 'user',
         },
       }),
     }),
